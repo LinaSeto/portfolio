e98ef298e5243dceff2718b1b1d7209c6f4defdc
LinaSeto <lseto@ucsd.edu>
2026-05-08 16:11:06 -0700

    add code stats and plot for meta page

---
+++ meta/main.js
@@ -0,0 +1,206 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+// read csv
+async function loadData() {
+    const data = await d3.csv('loc.csv', (row) => ({
+        ...row,
+        line: Number(row.line), // or just +row.line
+        depth: Number(row.depth),
+        length: Number(row.length),
+        date: new Date(row.date + 'T00:00' + row.timezone),
+        datetime: new Date(row.datetime),
+    }));
+
+    return data;
+}
+
+function processCommits(data) {
+    return d3
+        .groups(data, (d) => d.commit)
+        .map(([commit, lines]) => {
+            let first = lines[0];
+            let { author, date, time, timezone, datetime } = first;
+            let ret = {
+                id: commit,
+                url: 'https://github.com/LinaSeto/portfolio/commit/' + commit,
+                author,
+                date,
+                time,
+                timezone,
+                datetime,
+                // Calculate hour as a decimal for time analysis
+                hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+                // How many lines were modified?
+                totalLines: lines.length,
+            };
+
+            Object.defineProperty(ret, 'lines', {
+                value: lines,
+                configurable: false,
+                writable: false,
+                enumerable: false,
+            });
+
+            return ret;
+        });
+}
+
+// display stats
+function renderCommitInfo(data, commits) {
+    // Create the dl element
+    const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+
+    // Add total LOC
+    dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+    dl.append('dd').text(data.length);
+
+    // Add total commits
+    dl.append('dt').text('Commits');
+    dl.append('dd').text(commits.length);
+
+    // Add average line length
+    dl.append('dt').text('Avg Line Length');
+    dl.append('dd').text(d3.mean(data, d => d.length).toFixed(2));
+
+    // Add number of files
+    dl.append('dt').text('Files');
+    dl.append('dd').text(d3.group(data, d => d.file).size);
+
+    // Add average file depth
+    dl.append('dt').text('Avg File Depth');
+    const fileDepths = d3.rollups(
+        data,
+        (v) => d3.mean(v, (d) => d.depth),
+        (d) => d.file,
+    );
+    dl.append('dd').text(d3.mean(fileDepths, (d) => d[1]).toFixed(2));
+
+    // time of day most work is done
+    dl.append('dt').text('Peak Time');
+    const workByPeriod = d3.rollups(
+        data,
+        (v) => v.length,
+        (d) => new Date(d.datetime).toLocaleString('en', { dayPeriod: 'short' }),
+    );
+    dl.append('dd').text(d3.greatest(workByPeriod, (d) => d[1])?.[0]);
+
+
+}
+
+// tooltip
+function renderTooltipContent(commit) {
+    const link = document.getElementById('commit-link');
+    const date = document.getElementById('commit-date');
+
+    if (Object.keys(commit).length === 0) return;
+
+    link.href = commit.url;
+    link.textContent = commit.id;
+    date.textContent = commit.datetime?.toLocaleString('en', {
+        dateStyle: 'full',
+    });
+}
+
+function updateTooltipVisibility(isVisible) {
+  const tooltip = document.getElementById('commit-tooltip');
+  tooltip.hidden = !isVisible;
+}
+
+function updateTooltipPosition(event) {
+  const tooltip = document.getElementById('commit-tooltip');
+  tooltip.style.left = `${event.clientX}px`;
+  tooltip.style.top = `${event.clientY}px`;
+}
+
+// visualize time/day of commit
+function renderScatterPlot(data, commits) {
+    const width = 1000;
+    const height = 600;
+    const svg = d3
+        .select('#chart')
+        .append('svg')
+        .attr('viewBox', `0 0 ${width} ${height}`)
+        .style('overflow', 'visible');
+
+    const xScale = d3
+        .scaleTime()
+        .domain(d3.extent(commits, (d) => d.datetime))
+        .range([0, width])
+        .nice();
+
+    const yScale = d3.scaleLinear().domain([0, 24]).range([0, height]);
+
+    const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+    const usableArea = {
+        top: margin.top,
+        right: width - margin.right,
+        bottom: height - margin.bottom,
+        left: margin.left,
+        width: width - margin.left - margin.right,
+        height: height - margin.top - margin.bottom,
+    };
+
+    // Update scales with new ranges
+    xScale.range([usableArea.left, usableArea.right]);
+    yScale.range([usableArea.top, usableArea.bottom]);
+
+    // Create the axes
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3
+        .axisLeft(yScale)
+        .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
+
+    // color points
+    const colorScale = d3.scaleLinear()
+        .domain([0, 6, 12, 18, 24])
+        .range(['#1e3a8a', '#3b82f6', '#fbbf24', '#3b82f6', '#1e3a8a'])
+        .interpolate(d3.interpolateLab);
+
+    // Add gridlines
+    const gridlines = svg
+        .append('g')
+        .attr('class', 'gridlines')
+        .attr('transform', `translate(${usableArea.left}, 0)`);
+
+    // Create gridlines as an axis with no labels and full-width ticks
+    gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+
+    // Add X axis
+    svg
+        .append('g')
+        .attr('transform', `translate(0, ${usableArea.bottom})`)
+        .call(xAxis);
+
+    // Add Y axis
+    svg
+        .append('g')
+        .attr('transform', `translate(${usableArea.left}, 0)`)
+        .call(yAxis);
+
+    const dots = svg.append('g').attr('class', 'dots');
+
+    dots
+        .selectAll('circle')
+        .data(commits)
+        .join('circle')
+        .attr('cx', (d) => xScale(d.datetime))
+        .attr('cy', (d) => yScale(d.hourFrac))
+        .attr('r', 5)
+        // .attr('fill', 'steelblue')
+        .attr('fill', (d) => colorScale(d.hourFrac))
+        .on('mouseenter', (event, commit) => {
+            renderTooltipContent(commit);
+            updateTooltipVisibility(true);
+            updateTooltipPosition(event);
+        })
+        .on('mouseleave', () => {
+            updateTooltipVisibility(false);
+        });
+    ;
+}
+
+let data = await loadData();
+let commits = processCommits(data);
+
+renderCommitInfo(data, commits);
+renderScatterPlot(data, commits);
